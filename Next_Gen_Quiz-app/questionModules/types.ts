69ceff6a0a7a3477ab539aca683766669c607481
Sheikh <smujtabaja@gmail.com>
2024-08-11 18:37:01 +0500

    Num Guesssing Game

---
+++ Next_Gen_Quiz-app/questionModules/types.ts
@@ -0,0 +1,6 @@
+// types.ts
+export interface Question {
+    question: string;
+    options: string[];
+    correctAnswer: string;
+}
--- cli_number_guessing_game/index.js
+++ cli_number_guessing_game/index.js
@@ -5,21 +5,20 @@ import inquirer from "inquirer";
 const randomnumber = Math.floor(Math.random() * 10 + 1);
 // Collect the user's guessclear
 const answers = await inquirer.prompt([
-    {
-        // Set the name of the user's input as 'userGussedNumber'
-        name: "userGussedNumber",
-        // Specify the type of user input as a number
-        type: "number",
-        // Display the input message to the user
-        message: "Please guess a number between 1 to 10:",
-    },
+  {
+    // Set the name of the user's input as 'userGussedNumber'
+    name: "userGussedNumber",
+    // Specify the type of user input as a number
+    type: "number",
+    // Display the input message to the user
+    message: "Please guess a number between 1 to 10:",
+  },
 ]);
 // Check if the user's guess matches the random number
 if (answers.userGussedNumber === randomnumber) {
-    console.log("Congratulations! You guessed the correct number!");
-}
-else {
-    // If the user's guess is incorrect, let them know and display the correct number
-    console.log("Sorry, You guess wrong number");
-    console.log(`Correct number is ${randomnumber}`);
+  console.log("Congratulations! You guessed the correct number!");
+} else {
+  // If the user's guess is incorrect, let them know and display the correct number
+  console.log("Sorry, You guess wrong number");
+  console.log(`Correct number is ${randomnumber}`);
 }
